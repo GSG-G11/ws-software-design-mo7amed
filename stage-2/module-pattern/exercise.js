174338e7cf93ca61f89231708e7e92b18c9d4ccf
mo7amedehab97 <gazacityface@gmail.com>
2022-02-14 08:26:09 +0200

    exercise soulution

--- stage-2/module-pattern/exercise.js
+++ stage-2/module-pattern/exercise.js
@@ -1,10 +1,10 @@
-/*
- * Exercise: Create some modules!
- *
- * When you think you have finished, run the command:
- *   npm run s2.modules
- * This will run a series of tests which should all pass.
- */
+// *
+//  * Exercise: Create some modules!
+//  *
+//  * When you think you have finished, run the command:
+//  *   npm run s2.modules
+//  * This will run a series of tests which should all pass.
+//  */
 'use strict';
 
 /*
@@ -17,25 +17,31 @@
  * |          |     |                |   |      |  |             |
  * | protocol |     |    domain      |   | path |  | querystring |
  */
-var UrlParser = (function () {
-  // fill in ...
 
+var UrlParser = (function (url) {
+  function protocol(url) {
+    var proto = new URL(url).protocol.split("").splice(0, 5).join("");
+    return proto;
+  }
+  function domain(url) {
+    let domain = new URL(url).host;
+    return domain;
+  }
+  function path(url) {
+    let pathname = new URL(url).pathname.split("/").join("");
+    return pathname;
+  }
+  function querystring(url) {
+    let query = new URL(url).search.split("?").join("");
+    return query;
+  }
   return {
-    // a function that takes a URL and returns its protocol
-    protocol: null,
-
-    // a function that takes a URL and returns its domain
-    domain: null,
-
-    // a function that takes a URL and returns its path
-    path: null,
-
-    // a function that takes a URL and returns its query string
-    querystring: null,
+    protocol,
+    domain,
+    path,
+    querystring,
   };
-});
-
-
+})("http://www.mymainsite.com/somepath/path2/path3/path4 ?foo=1&bar=2");
 /*
  * Create a module that can support multiple instances (like in our example).
  * The module should be a function with several additional methods attached as
@@ -51,10 +57,15 @@ var UrlParser = (function () {
  * exampleBuilder.
  */
 var createUrlBuilder = function (host) {
-  // fill in ...
-
-  var builder = function () {}
-
+  // let url = "https://example.com/hello?foo=1&bar=2";
+  var getQueryStr = query => Object.entries(query).map(entArr => entArr.join("=")).join('&');
+  var builder = function ({ query, path }) {
+    return `${builder.path(path)}?${getQueryStr(query)}`
+  }
+  builder.path = path => `${host}/${path}`
+  builder.query = query => {
+    return `${host}?${getQueryStr(query)}`
+  }
   return builder;
 };
 
